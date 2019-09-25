c0b78d1fb6765be908923d27ae06a6a9d2eeb1b7
macstudent <fmsharmn@gmail.com>
2019-09-25 18:37:19 -0400

    binary

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -2,13 +2,26 @@ package com.lambton;
 
 public class LambtonStringTools {
 
-    public String reverse(String string) {
+    public String reverse(String string)
+    {
         StringBuilder rev = new StringBuilder();
-        for (int i = string.length() - 1; i >=0; i--) {
+        for (int i = string.length() - 1; i >=0; i--)
+        {
             rev.append(string.charAt(i));
         }
+
         return rev.toString();
-        }
+    }
+     public int binaryToDecimal(String binary)
+     {
+         int decimal = 0;
+         int binaryMultiplier = 1;
+         for(char c:reverse(binary).toCharArray())
+         {
+
+         }
+
+     }
     }
 
 
