6b1f12f597e6ef0313c7ba8b01af756f094f3ac8
macstudent <fmsharmn@gmail.com>
2019-09-25 18:19:07 -0400

    reverse full

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -5,7 +5,9 @@ public class LambtonStringTools {
     public String reverse(String string) {
         StringBuilder rev = new StringBuilder();
         for (int i = string.length() - 1; i > 0; i++) {
-
+            rev.append(string.charAt(i));
+        }
+        return rev.toString();
         }
     }
 
